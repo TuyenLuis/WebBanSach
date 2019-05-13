257de5863e3db68fffc6a8e0e0c0cee4871fe64a
mjnk9xw <minhnguyen998vp@gmail.com>
2019-05-14 06:52:32 +0700

    commit login fb and security pass

--- WebBanSach/Controllers/DangKiController.cs
+++ WebBanSach/Controllers/DangKiController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebBanSach.Models.EF;
+using WebBanSach.Models.Security;
 
 namespace WebBanSach.Controllers
 {
     public class DangKiController : Controller
     {
-        QuanLyBanSachDbContext db = new QuanLyBanSachDbContext();
+        private QuanLyBanSachDbContext db = new QuanLyBanSachDbContext();
+        private SecurityPassWord sp = new SecurityPassWord();
+
         // GET: DangKi
         public ActionResult Index()
         {
@@ -19,17 +22,25 @@ namespace WebBanSach.Controllers
         {
             try
             {
-                user.Quyen = 2;
-                db.Khachhangs.Add(user);
-                db.SaveChanges();
-                
-                if (Session["USER_SESSION"] == null)
+                Khachhang khachHang = db.Khachhangs.SingleOrDefault(item => item.Email == user.Email);
+                if (khachHang == null)
                 {
-                    Session.Add("USER_SESSION", user);
-                }
-                else
-                {
-                    Session["USER_SESSION"] = user;
+                    //process pass
+                    string pass_hash = sp.HashPass(user.Matkhau);
+                    user.Matkhau = pass_hash;
+
+                    user.Quyen = 2;
+                    db.Khachhangs.Add(user);
+                    db.SaveChanges();
+
+                    if (Session["USER_SESSION"] == null)
+                    {
+                        Session.Add("USER_SESSION", user);
+                    }
+                    else
+                    {
+                        Session["USER_SESSION"] = user;
+                    }
                 }
                 return Json(new { status = true });
             }
--- WebBanSach/Controllers/DangNhapController.cs
+++ WebBanSach/Controllers/DangNhapController.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using Facebook;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using WebBanSach.Models.Common;
 using WebBanSach.Models.EF;
+using WebBanSach.Models.Security;
 
 namespace WebBanSach.Controllers
 {
     public class DangNhapController : Controller
     {
-        QuanLyBanSachDbContext db = new QuanLyBanSachDbContext();
+        private QuanLyBanSachDbContext db = new QuanLyBanSachDbContext();
+        private SecurityPassWord sp = new SecurityPassWord();
         // GET: DangNhap
         public ActionResult Index()
         {
@@ -29,7 +33,8 @@ namespace WebBanSach.Controllers
         {
             if (ModelState.IsValid)
             {
-                Khachhang khachHang = db.Khachhangs.SingleOrDefault(item => item.Tendn == user.Username && item.Matkhau == user.Password);
+                string pass_hash = sp.HashPass(user.Password);
+                Khachhang khachHang = db.Khachhangs.SingleOrDefault(item => item.Tendn == user.Username && item.Matkhau == pass_hash);
                 if (khachHang == null)
                 {
                     ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng. Xin kiểm tra lại!");
@@ -41,6 +46,112 @@ namespace WebBanSach.Controllers
             }
             return View("Index");
         }
+
+        // beign fb
+        private Uri RedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult loginFB()
+        {
+            return View();
+        }
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return View("Login");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Facebook()
+        {
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = "530116780853982",
+                client_secret = "380cfe5f0ab22004ce2ff07d3c6c5017",
+                redirect_uri = RedirectUri.AbsoluteUri,
+                response_type = "code",
+                scope = "email" // Add other permissions as needed
+            });
+
+            return Redirect(loginUrl.AbsoluteUri);
+        }
+
+        public ActionResult FacebookCallback(string code)
+        {
+            var fb = new FacebookClient();
+            dynamic result = fb.Post("oauth/access_token", new
+            {
+                client_id = "530116780853982",
+                client_secret = "380cfe5f0ab22004ce2ff07d3c6c5017",
+                redirect_uri = RedirectUri.AbsoluteUri,
+                code = code
+            });
+
+            var accessToken = result.access_token;
+
+            // Store the access token in the session for farther use
+            Session["AccessToken"] = accessToken;
+
+            // update the facebook client with the access token so 
+            // we can make requests on behalf of the user
+            fb.AccessToken = accessToken;
+
+            // Get the user's information
+            dynamic me = fb.Get("me?fields=id,name,email,birthday,gender");
+            string email = me.email;
+            string name = me.name;
+            string birthday = me.birthday;
+            string gender = me.gender; 
+
+
+
+            Khachhang khachHang = db.Khachhangs.SingleOrDefault(item => item.Email == email);
+            if (khachHang == null)
+            {
+                // set khach hang login fb
+                khachHang = new Khachhang();
+                khachHang.Email = email;
+                khachHang.Hotenkh = name;
+                khachHang.Quyen = 2;
+                khachHang.Ngaysinh = Convert.ToDateTime( birthday);
+                if (gender == "male")
+                {
+                    khachHang.Gioitinh = true;
+                }
+                else
+                {
+                    khachHang.Gioitinh = false;
+                }
+                
+
+                db.Khachhangs.Add(khachHang);
+                db.SaveChanges();
+
+                Session.Add("USER_SESSION", khachHang);
+            }
+            else
+            {
+                Session.Add("USER_SESSION", khachHang);
+            }
+
+            // Set the auth cookie
+            FormsAuthentication.SetAuthCookie(email, false);
+            return RedirectToAction("Index", "Home");
+        }
+
+        // end fb
 
         public ActionResult DangXuat()
         {
