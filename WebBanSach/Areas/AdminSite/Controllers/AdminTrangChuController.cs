335046148b6b9d07e14c2468304b4961d0fa3487
Hiep Nguyen <hiep.nguyenngoc@powergatesoftware.net>
2019-06-29 23:25:52 +0700

    fix bug thống kê

--- WebBanSach/Areas/AdminSite/Controllers/AdminTrangChuController.cs
+++ WebBanSach/Areas/AdminSite/Controllers/AdminTrangChuController.cs
@@ -23,11 +23,32 @@ namespace WebBanSach.Areas.AdminSite.Controllers
         [HttpPost]
         public JsonResult ThongKe()
         {
-            List<ThongKe> tk = db.Database.SqlQuery<ThongKe>("SELECT sum(Tongtien) as TongTien from Giohangkh where YEAR(Ngaymua) = YEAR(GETDATE()) group by MONTH(Ngaymua) order by MONTH(Ngaymua)").ToList();
-            decimal[] Tien = new decimal[12];
-            for (int i = 0; i < tk.Count; i++)
+            //List<ThongKe> DuLieu = new List<ThongKe>();
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    ThongKe DL = new ThongKe();
+            //    DL.Thang = i;
+            //    DL.TongTien = 0;
+            //    DuLieu.Add(DL);
+            //}
+            List<ThongKe> DuLieu = db.Database.SqlQuery<ThongKe>("SELECT sum(Tongtien) as TongTien, MONTH(Ngaymua) as Thang from Giohangkh where YEAR(Ngaymua) = YEAR(GETDATE()) group by MONTH(Ngaymua) order by MONTH(Ngaymua)").ToList();
+            decimal[] Tien = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //for (int i = 0; i < DuLieu.Count; i++)
+            //{
+            //    if(DuLieu[i].Thang == i)
+            //    {
+            //        Tien[i] = DuLieu[i].TongTien;
+            //    }
+            //}
+            foreach(ThongKe t in DuLieu)
             {
-                Tien[i] = tk[i].TongTien;
+                for (int i = 1; i <= 12; i++)
+                {
+                    if (t.Thang == i)
+                    {
+                        Tien[i] = t.TongTien;
+                    }
+                }
             }
             return Json(Tien);
         }
@@ -36,5 +57,6 @@ namespace WebBanSach.Areas.AdminSite.Controllers
     {
         public int Thang { get; set; }
         public decimal TongTien { get; set; }
+        
     }
 }
